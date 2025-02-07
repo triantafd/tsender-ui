fe4dd09b1419ecf6a53796d7a5ec3b55dc69f0d5
Patrick Collins <54278053+PatrickAlphaC@users.noreply.github.com>
2025-02-06 19:10:47 -0500

    wip

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,6 @@ import { FaGithubSquare } from "react-icons/fa";
 
 export default function Header() {
     return (
-
         <nav className="p-4 border-b-2 flex flex-row justify-between items-center">
             <h1 className="py-2 px-4 font-bold text-2xl">TSender</h1>
             <h3 className="italic text-left">The most gas efficient airdrop contract on earth, built in huff 🐎</h3>
