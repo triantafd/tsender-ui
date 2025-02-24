21cacaa4c719082297b7fcd102a7d66ee812ecc7
Patrick Collins <54278053+PatrickAlphaC@users.noreply.github.com>
2025-02-24 15:48:52 -0500

    Revert "wip"
    This reverts commit 2e736e6a5b469fc292df068b80aedbfe05b08b80.

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,12 +12,12 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: 'test',
+  testDir: 'e2e',
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? parseInt(`${process.env.WORKERS}`, 10) : 1,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
--- vitest.config.mts
+++ vitest.config.mts
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [tsconfigPaths()],
     test: {
         environment: 'jsdom',
-        exclude: ['**/node_modules/**', '**/test/**', 'playwright-report/**', 'test-results/**'],
+        exclude: ['**/node_modules/**', '**/e2e/**'],
         deps: {
             inline: ['wagmi', '@wagmi/core']
         }
